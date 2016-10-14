0e801cf79643c953eadc13a3e0c1cc065197b776
Coty Embry <cotyembry@gmail.com>
2016-10-13 22:47:39 -0500

    finished Script_1.1.sh - ready to be turned it

--- Script_1.1.sh
+++ Script_1.1.sh
@@ -3,15 +3,24 @@
 #Class: 	Unix System Administration
 #Program Comment:	This will rename all files in a subdirectory called Script_1 to prepend and append the letter X to the file name
 
-#...
 #!/usr/local/bin/bash
 
 directory='Script_1'
 
-for file in `ls ./$directory`
-	do
-		#`mv $file ./X${file}X`
-		echo $file
-	done
+# for file in `ls ./$directory` #another - less predictable apparently - way to iterate through files
 
+# I will use absolute paths
+for fileName in ./$directory/*; do
+	fileName=$(echo $fileName | cut -d "/" -f3)
+	# fileName=$(echo $fileName | cut -d "/" -f3)
+	___dirname=$(pwd)
+	mv $___dirname/${directory}/$fileName $___dirname/${directory}/X${fileName}X
+	# cp $fileName 
 
+done
+
+		# echo $fileName
+		# echo $file
+
+		# `mv ./$directory/$file ./X${file}X`
+		# echo $file
